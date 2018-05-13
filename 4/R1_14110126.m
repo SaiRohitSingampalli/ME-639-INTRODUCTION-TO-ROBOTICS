922be5beab5a3e80ff37018065a9d5ec15a44f89
SAI ROHIT SINGAMPALLI <37893685+SaiRohitSingampalli@users.noreply.github.com>
2018-05-13 14:22:09 +0530

    Add files via upload

---
+++ 4/R1_14110126.m
@@ -0,0 +1,138 @@
+clc; clear all;
+l1 = 1;                                               l2 = 1;
+lc1 = l1/2;                                           lc2 = l2/2;
+m1 = 1;            
+m2 = 1;
+syms t
+r1 = 1/100; r2 = r1;
+ti = 0;     tf = 2;
+disp('Please be patient, processing is going on')
+%equations for fitting in cubic equation
+time(1,1) = 1;                 time(2,1) = 0;                time(3,1) = 1;                   time(4,1) = 0;
+time(1,2) = ti;                time(2,2) = 1;                time(3,2) = tf;                  time(4,2) = 1;
+time(1,3) = ti^2;              time(2,3) = 2*ti;             time(3,3) = tf^2;                time(4,3) = 2*tf;
+time(1,4) = ti^3;              time(2,4) = 3*(ti^2);         time(3,4) = tf^3;                time(4,4) = 3*(tf^2);
+Q(1,1) = 0;                    Q(2,1) = 0;                   Q(3,1) = pi/6;                   Q(4,1) = 0;
+R(1,1) = 0;                    R(2,1) = 0;                   R(3,1) = pi/3;                   R(4,1) = 0;
+A = (inv(time))*Q;             B = (inv(time))*R;
+
+i = 1;
+for t = 0:0.020:2
+    QQ1(i) = A(1,1) + A(2,1)*t + A(3,1)*(t^2) + A(4,1)*(t^3);
+    QQd1(i) = A(2,1) + 2*A(3,1)*t + 3*A(4,1)*(t^2);
+    QQ2(i) = B(1,1) + B(2,1)*t + B(3,1)*(t^2) + B(4,1)*(t^3);
+    QQd2(i) = B(2,1) + 2*B(3,1)*t + 3*B(4,1)*(t^2);
+    QQdd1(i) = 2*A(3,1) + 6*A(4,1)*t;
+    QQdd2(i) = 2*B(3,1) + 6*B(4,1)*t;  
+    %T1(i) = (49*cos(QQ1(i) + QQ2(i)))/10 + (147*cos(QQ1(i)))/10 + QQdd1(i)*(cos(QQ2(i)) + 5/3) + QQdd2(i)*(cos(QQ2(i))/2 + 1/3) - QQd2(i)*((QQd1(i)*sin(QQ2(i)))/2 + (QQd2(i)*sin(QQ2(i)))/2) - (QQd1(i)*QQd2(i)*sin(QQ2(i)))/2;
+    %T2(i) = (sin(QQ2(i))*QQd1(i)^2)/2 + QQdd2(i)/3 + (49*cos(QQ1(i) + QQ2(i)))/10 + QQdd1(i)*(cos(QQ2(i))/2 + 1/3);
+    i = i+1;
+end
+
+%plotting joint angles and velocities
+t = 0 : 0.020 : 2;
+figure(1);
+plot(t,QQ1,'r');
+hold on 
+plot(t,QQd1,'b');
+hold on
+plot(t,QQdd1,'y');
+legend('QQ1','QQd1','QQdd1');
+xlabel('time');
+ylabel('variation of q1, q1d, q1dd');
+figure(2);
+plot(t,QQ2,'r');
+hold on 
+plot(t,QQd2,'b');
+hold on
+plot(t,QQdd2,'y');
+legend('QQ2','QQd2','QQdd2');
+xlabel('time');
+ylabel('variation of q2, q2d, q2dd');
+%figure(3);
+%plot(t,T1,'b');
+%hold on
+%plot(t,T2,'r');
+%legend('T1','T2');
+%xlabel('time');
+%ylabel('Torques Value');
+%animation
+p = [0 0];
+figure(3);
+for i = 1 : 1 : 100
+    x = l1*cos(QQ1(i)) + l2*cos(QQ1(i)+QQ2(i));
+    y = l1*sin(QQ1(i)) + l2*sin(QQ1(i)+QQ2(i));
+    plot(0,0,'r.');
+    plot(x,y,'r.');
+    link1 = line([p(1) l1 * cos(QQ1(i)) ],[p(2) l1 * sin(QQ1(i))],'LineWidth',2);
+    link2 = line([l1 * cos(QQ1(i)) x],[l1 * sin(QQ1(i)) y],'LineWidth',4);
+    hold on
+    pause(0.05)
+    delete(link1)
+    delete(link2)
+    axis([-2 2 -2 2]);
+    hold on
+end 
+
+
+[t,q] = ode45('controlpd',[0 2],[0 0 0 0 0 0]);
+
+for i = 1:1:101
+  
+    PQ1(i) = -r1*q(i,1);
+    PQ2(i)=  r1*q(i,2);
+    PQ3(i) = -r2*q(i,3);
+    PQ4(i) = r2*q(i,4);
+end
+t = 0:0.02:2;
+
+figure(4);
+plot(t,PQ1,'b');
+hold on
+plot(t,QQ1,'r');
+legend('Simulated','Desired');
+xlabel('time');
+ylabel('q values');
+
+figure(5);
+plot(t,PQ3,'b');
+hold on
+plot(t,QQ2,'r');
+legend('Simulated','Desired');
+xlabel('time');
+ylabel('q values');
+
+figure(6);
+plot(t,PQ2,'b');
+hold on
+plot(t,QQd1,'r');
+legend('Simulated','Desired');
+xlabel('time');
+ylabel('q dot values');
+
+figure(7);
+plot(t,PQ1,'b');
+hold on
+plot(t,QQd2,'r');
+legend('Simulated','Desired');
+xlabel('time');
+ylabel('q dot  values');
+
+p = [0 0];
+figure(8);
+for i = 1 : 1 : 100
+    x = l1*cos(PQ1(i)) + l2*cos(PQ1(i)+PQ3(i));
+    y = l1*sin(PQ1(i)) + l2*sin(PQ1(i)+PQ3(i));
+    plot(0,0,'r.');
+    plot(x,y,'r.');
+    link1 = line([p(1) l1 * cos(PQ1(i)) ],[p(2) l1 * sin(PQ1(i))],'LineWidth',2);
+    link2 = line([l1 * cos(PQ1(i)) x],[l1 * sin(PQ1(i)) y],'LineWidth',4);
+    hold on
+    pause(0.05)
+    delete(link1)
+    delete(link2)
+    axis([-2 2 -2 2]);
+    hold on
+end 
+
+
---
+++ 4/controlpd.m
@@ -0,0 +1,103 @@
+function controlterms = controlpd(t,q)
+%inputs
+l1 = 1;                                               l2 = 1;
+lc1 = l1/2;                                           lc2 = l2/2;
+m1 = 1;                                               m2 = 1;
+i1 = 1/12*(m1*l1*l1);                                 i2 = 1/12*(m2*l2*l2);
+g = 9.8; r1 = 1/100;                                          r2 = r1; 
+% syms QQ1 QQ2 QQd1 QQd2 QQdd1 QQdd2 t
+ti = 0;                                               tf = 2;
+%equations for fitting in cubic equation
+time(1,1) = 1;                time(2,1) = 0;                     time(3,1) = 1;                     time(4,1) = 0;
+time(1,2) = ti;               time(2,2) = 1;                     time(3,2) = tf;                    time(4,2) = 1;
+time(1,3) = ti^2;             time(2,3) = 2*ti;                  time(3,3) = tf^2;                  time(4,3) = 2*tf;
+time(1,4) = ti^3;             time(2,4) = 3*(ti^2);              time(3,4) = tf^3;                  time(4,4) = 3*(tf^2);
+Q(1,1) = 0;                   Q(2,1) = 0;                        Q(3,1) = pi/6;                     Q(4,1) = 0;
+R(1,1) = 0;                   R(2,1) = 0;                        R(3,1) = pi/3;                     R(4,1) = 0;
+A = (inv(time))*Q;            B = (inv(time))*R;
+
+QQ1= A(1,1) + A(2,1)*t + A(3,1)*(t^2) + A(4,1)*(t^3);
+QQd1= A(2,1) + 2*A(3,1)*t + 3*A(4,1)*(t^2);
+QQ2= B(1,1) + B(2,1)*t + B(3,1)*(t^2) + B(4,1)*(t^3);
+QQd2= B(2,1) + 2*B(3,1)*t + 3*B(4,1)*(t^2);
+QQdd1= 2*A(3,1) + 6*A(4,1)*t;
+QQdd2= 2*B(3,1) + 6*B(4,1)*t;  
+
+qqqq1 = [A(4,1) A(3,1) A(2,1) A(1,1)];
+cosqqqq1 = @(t) cos(A(4,1)*t.^3 + A(3,1)*t.^2 + A(2,1)*t.^1 + A(1,1)*t.^0);
+sinqqqq1 = @(t) sin(A(4,1)*t.^3 + A(3,1)*t.^2 + A(2,1)*t.^1 + A(1,1)*t.^0);
+cosqqqq1_avg = integral(cosqqqq1,0,2)/2;
+sinqqqq1_avg = integral(sinqqqq1,0,2)/2;
+
+qqqq2 = [B(4,1) B(3,1) B(2,1) B(1,1)];
+cosqqqq2 = @(t) cos(B(4,1)*t.^3 + B(3,1)*t.^2 + B(2,1)*t.^1 + B(1,1)*t.^0);
+sinqqqq2 = @(t) sin(B(4,1)*t.^3 + B(3,1)*t.^2 + B(2,1)*t.^1 + B(1,1)*t.^0);
+cosqqqq2_avg = integral(cosqqqq2,0,2)/2;
+sinqqqq2_avg = integral(sinqqqq2,0,2)/2;
+%D11avg = avg(cos(r2*q2_des)) + 5/3;
+D11 = @(t) cos(r2*(B(4,1)*t.^3 + B(3,1)*t.^2 + B(2,1)*t.^1 + B(1,1)*t.^0));
+D11_avg = integral(D11,0,2)/2;
+D22 = 1/3;
+
+Jm1 = 0.4*(10^-4);                                   Jm2 = Jm1;
+Km1 = 2.32*(10^-2);                                  Km2 = Km1;
+Bm1 = 4.77*(10^-5);                                  Bm2 = Bm1;
+R1 = 0.365;                                          R2 = R1;
+Kb1 = 0.0232;                                        Kb2 = Kb1;
+r1 = 1/100;                                          r2 = r1;
+w1 = 4;                                              w2 = w1;
+Geta1 = 1;                                           Geta2 = Geta1;
+K1 = Km1/R1;                                         K2 = Km2/R2;
+B1 = Bm1 +(Kb1*Km1/R1);                              B2 = Bm2 +(Kb2*Km2/R2);
+J1 = Jm1 + (r1*r1*(D11_avg + (5/3)));                              
+J2 = Jm2 + r2*r2*D22;
+Kp1 = (w1*w1*J1)/K1;                                Kp2 = (w2*w2*J2)/K2;
+Kd1 = 2*Geta1*w1*J1 - (B1/K1);                      Kd2 = 2*Geta2*w2*J2 - (B2/K2);
+Ki1 = 0; Ki2 = 0;
+
+
+
+d11 = cos(r2*q(3)) + 5/3;
+d12 = cos(r2*q(3))/2 + 1/3;
+d21 = d12;
+d22 = 1/3;
+
+h = -(sin(r2*q(3)))/2;
+c111 = 0;
+c121 = h;
+c211 = h;
+c221 = h;
+c112 = -h;
+c122 = 0;
+c212 = 0;
+c222 = 0;
+
+
+phi1 = (g*cos(r1*q(1) + r2*q(3)))/2 + (3*g*cos(r1*q(1)))/2;
+phi2 = (g*cos(r1*q(1) + r2*q(3)))/2;
+
+rsd1 = c121*r1*r2*q(2)*q(4) + c211*r1*q(2)*r2*q(4)+c221*(r2*q(4))^2 + phi1;
+rsd2 = c112*(r1*q(2))^2 + phi2;
+
+
+couple = [J1       r1*r2*d21;
+          r1*r2*d21 J2];
+
+V(1) = Kp1*(QQ1 - q(1)) - Kd1*q(2) + Ki1*q(5);
+V(2) = Kp2*(QQ2 - q(3)) - Kd2*q(4) + Ki2*q(6);
+
+CoupleRHS(1) = K1*V(1)-r1*rsd1-B1*q(2);
+CoupleRHS(2) = K2*V(2)-r2*rsd2-B2*q(4);
+
+QdotMatrix = inv(couple)*[CoupleRHS(1);CoupleRHS(2)];
+
+
+
+controlterms(1,1) = q(2);
+controlterms(2,1) = (QdotMatrix(1));
+controlterms(3,1) = q(4);
+controlterms(4,1) = (QdotMatrix(2));
+controlterms(5,1) = QQ1 - q(1);
+controlterms(6,1) = QQ2 - q(3);
+
+end
